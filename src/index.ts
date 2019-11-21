58af1629b9be6db20bc0a7058cb7bf1080040fb7
Paige Zaleppa <pz0342@gmail.com>
2019-11-20 20:17:22 -0500

    :tada: Adding functionality

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hi');
+});
+
+app.listen(port, () => {
+    // tslint:disable-next-line:no-console
+    console.log('started');
+});
